4b024ab116493a363cc1b7929dd13536d380392c
”majtan” <ma.novo.2023@alumnos.urjc.es>
2025-04-03 18:29:20 +0200

    Añadir practica terminada

--- copybytes.c
+++ copybytes.c
@@ -21,52 +21,91 @@ struct Files {
 
 typedef struct Files Files;
 
+struct Bytes {
+    ssize_t bytesRead;
+    ssize_t bytesWritten;
+    ssize_t result;
+};
+
+typedef struct Bytes Bytes;
+
+
+void safeExit(Files *files, Bytes *bytes) {
+    free(files);
+    free(bytes);
+}
+
 int main(int argc, char *argv[]) {
+    argc--;
+    argv++;
+    char buffer[BUFFER_SIZE];
 
+    Files *files = (Files *)malloc(sizeof(Files));
+    if (files == NULL) {
+        err(EXIT_FAILURE, "Error de memoria");
+    }
 
-    argc --;
-    argv ++;
-    
+    Bytes *bytes = (Bytes *)malloc(sizeof(Bytes));
+    if (bytes == NULL) {
+        free(files);
+        err(EXIT_FAILURE, "Error de memoria");
+    }
+
     if (argc < 2) {
-        fprintf(stderr, "Numero de argumentos invalido\n");
+        fprintf(stderr, "Número de argumentos inválido\n");
+        safeExit(files, bytes);
         exit(EXIT_FAILURE);
     }
 
-    Files *files = (Files *)malloc(sizeof(Files));
+    if (strcmp(argv[0], "-") == 0) {
+        argv[0] = "/dev/tty";
+    }
+
+    if (strcmp(argv[1], "-") == 0) {
+        argv[1] = "/dev/tty";
+    }
+
     files->file_read = argv[0];
     files->file_write = argv[1];
-    char buffer[BUFFER_SIZE];
 
-
-    // Abrir archivo de lectura
     files->fd_read = open(files->file_read, O_RDONLY);
     if (files->fd_read < 0) {
+        safeExit(files, bytes);
         err(EXIT_FAILURE, "No se puede abrir el archivo de lectura %s", files->file_read);
     }
 
-    // Abrir archivo de escritura
     files->fd_write = open(files->file_write, O_WRONLY | O_CREAT | O_TRUNC, 0666);
     if (files->fd_write < 0) {
         close(files->fd_read);
+        safeExit(files, bytes);
         err(EXIT_FAILURE, "No se puede abrir el archivo de escritura %s", files->file_write);
     }
 
-    // Leer y escribir en el nuevo archivo
-    ssize_t bytesRead;
-    while ((bytesRead = read(files->fd_read, buffer, BUFFER_SIZE)) > 0) {
-        if (write(files->fd_write, buffer, bytesRead) != bytesRead) {
-            err(EXIT_FAILURE, "Error al escribir en %s", files->file_write);
+    while ((bytes->bytesRead = read(files->fd_read, buffer, BUFFER_SIZE)) > 0) {
+        bytes->bytesWritten = 0;
+
+        while (bytes->bytesWritten < bytes->bytesRead) {
+            bytes->result = write(files->fd_write, buffer + bytes->bytesWritten, bytes->bytesRead - bytes->bytesWritten);
+            if (bytes->result < 0) {
+                safeExit(files,bytes);
+                close(files->fd_read);
+                close(files->fd_write);
+                err(EXIT_FAILURE, "Error al escribir en %s", files->file_write);
+            }
+
+            bytes->bytesWritten += bytes->result; 
+        }
+
+        if (bytes->bytesRead < 0) {
+            safeExit(files, bytes);
+            close(files->fd_read);
+            close(files->fd_write);
+            err(EXIT_FAILURE, "Error al leer de %s", files->file_read);
         }
     }
-    if (bytesRead < 0) {
-        err(EXIT_FAILURE, "Error al leer de %s", files->file_read);
-    }
 
-    // Cerrar archivos y liberar memoria
     close(files->fd_read);
     close(files->fd_write);
-    free(files);
+    safeExit(files, bytes);
     exit(EXIT_SUCCESS);
 }
-
-//Para terminar la lectura no se hace ctr+d sino ctrl+c
